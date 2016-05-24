929def74446fddd155aa5e7846b76bd10059d714
OptimisticPi <crawford.joye@gmail.com>
2016-05-23 19:44:38 -0700

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,4 @@
+consumer_key = "Iubtokrjpcj0dyGDZs0nRuUm3"
+consumer_secret = "XjO4FY1jGja6x4Kr7NJi7NsqAl5WjiTZSQPQDNWooGUR7yagiZ"
+access_token = "734934197178007552-ydoYzixl1pFS8up8UYcVmaDu2KkIyZ1"
+access_token_secret = "4wCBlLYCa6ZlsNu1TRwisIf8j5JIH67rLQqFRUkShcSz3"
